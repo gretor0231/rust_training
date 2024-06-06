f068bcda956fdeb56511d3ee34790877414561d1
gretor0231 <eric@barkingdog.ai>
2024-06-06 18:32:12 +0800

    master: 1. fn onwership testing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::ffi::CString;
+
 fn main() {
     println!("Hello, world!");
+
+    let s1 = 0965703320.to_string();
+    let s2 = "hello".to_string();
+
+
+    println!("s1 = {}", s1);
+    println!("s2 length = {}", calStringLen(&s2));
+
+}
+
+
+
+fn calStringLen(s: &String) -> usize {
+    return s.len()
 }
