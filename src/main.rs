21cbbb8b45e8c51394dab517392132550b29453b
gretor0231 <gretor0231@gmail.com>
2024-07-14 12:07:32 +0800

    training; ExerciseB

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@ fn main() {
     println!("Firing{} of my {} missiles...", ready, missiles);
 
 
+    let width =4;
+    let length = 5;
+    let depth =6;
+
+    // ExerciseB Functions
+    println!("{}", area(width, length, depth));
 }
 
+
+fn area(width: u32, length: u32, depth: u32) -> u32 {
+    return width * length * depth
+}
 
